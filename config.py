ccafb2ce92ff316f216651dcaecb9e9390ae5a6c
Brett LaBombarda <bplabombarda@gmail.com>
2015-11-02 21:27:57 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # =====================================================================
 # Twitter API Kajigger
 # =====================================================================
-consumer_key = 'kFDccpGLXPjdmvxa2YDizfZ88'
-consumer_secret = 'uZB61CIilJhIKegsDe1OS6Hk9nYgV9wMGakREAbEcIvGpK5Kbo'
-access_token = '4026302193-PtfGQ6OCO8zbUwhOelJpOJCOAU5WZceKgddvEVE'
-access_token_secret = '3Q7WwXagKVkSvvmIBSoEK6ZhfqoPFsawtSgNTVcb4mVRh'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
