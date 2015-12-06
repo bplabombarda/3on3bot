e84c8de35c961fd13148f1ba80a52dbf13abc3fd
Brett LaBombarda <bplabombarda@gmail.com>
2015-12-05 22:55:59 -0600

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-# SET DEBUG TO FALSE IN PRODUCTION LEST THE WHOLE FUCKING WORLD SEE OUR STACK TRACES
-# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-
-
-from app import app
-app.run(debug=True)
