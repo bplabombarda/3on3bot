9ea57b4d0a21f287e23bb50d17debba5eaa3bbf6
Brett LaBombarda <bplabombarda@gmail.com>
2015-12-18 14:50:51 -0800

    Update tweeter_v2.py
    Getting source ready for production.

--- tweeter_v2.py
+++ tweeter_v2.py
@@ -7,17 +7,17 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# res = requests.get('http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp')
-# data = res.text[15:-1]
-# json_data = json.loads(data)
-# games = json_data['games']
+res = requests.get('http://live.nhle.com/GameData/RegularSeasonScoreboardv3.jsonp')
+data = res.text[15:-1]
+json_data = json.loads(data)
+games = json_data['games']
 
 # =====================================================================
 # Testing shit
 # =====================================================================
-with open('critical_LAK.json', 'r') as data_file:
-    data = data_file.read()
-    games = json.loads(data)
+# with open('critical_LAK.json', 'r') as data_file:
+#     data = data_file.read()
+#     games = json.loads(data)
 
 # =====================================================================
 # Load hashtag JSON file
