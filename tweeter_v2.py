fa2fc0f7b7e5dc6fce90f69d438e1b6358954c21
Brett LaBombarda <bplabombarda@gmail.com>
2015-12-18 14:47:20 -0800

    Still on master for some reason...last one I promise.

--- tweeter_v2.py
+++ tweeter_v2.py
@@ -3,11 +3,6 @@ import json
 import tweepy
 import requests
 
-consumer_key = "KwL1hHqnwf6hnzbJwJSnQRNdB"
-consumer_secret = "BVpG4vWU5FdbQrbqg1OUwBroa3CGYOX2TbVjbV4XbvnM8lURBs"
-access_token = "4026302193-84IorOoSRTUy7zzqAt9hQt1RgtMMnGfuC5QPUpc"
-access_token_secret = "mTR80znoNcQqmlFFyBEKHYf23lB2MravbSipsvnEtoMlV"
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
@@ -20,53 +15,71 @@ api = tweepy.API(auth)
 # =====================================================================
 # Testing shit
 # =====================================================================
-with open('ot_SJS.json', 'r') as data_file:
+with open('critical_LAK.json', 'r') as data_file:
     data = data_file.read()
     games = json.loads(data)
 
+# =====================================================================
+# Load hashtag JSON file
+# =====================================================================
+with open('hashtags.json', 'r') as ht_file:
+    data = ht_file.read()
+    hashtags = json.loads(data)
 
-def parse_game(game):
+
+def parse_game(game, hashtags):
 
     game_prog = game['tsc']
     game_status = game['bs']
     game_period = game['ts']
-    away_team_name = game['atn']
+    away_team_name = game['atv']
     away_team_score = game['ats']
-    home_team_name = game['htn']
+    home_team_name = game['htv']
     home_team_score = game['hts']
 
-    if home_team_name == 'MontrĂŠal':
-        home_team_name = 'Montreal'
+    # =====================================================================
+    # Find team hashtags
+    # =====================================================================
+    for hashtag in hashtags:
+        if hashtag['team_name'] == away_team_name:
+            away_team_ht = hashtag['hashtag']
 
-    elif away_team_name == 'MontrĂŠal':
-        away_team_name = 'Montreal'
+        if hashtag['team_name'] == home_team_name:
+            home_team_ht = hashtag['hashtag']
 
-
-    if game['tsc'] == 'progress' and \
+    # =====================================================================
+    # Check END 3RD
+    # =====================================================================
+    if game['tsc'] == 'critical' and \
         game['bs'] == 'LIVE' and \
         game['ts'] == 'END 3rd':
-        pass
-        # tweet_game(api, away_team_name, away_team_score,
-        #             home_team_name, home_team_score)
 
-    elif game['tsc'] == 'progress' and \
+        tweet_game(api, away_team_ht, away_team_score,
+                    home_team_ht, home_team_score)
+
+    # =====================================================================
+    # Check OT
+    # =====================================================================
+    elif game['tsc'] == 'critical' and \
         game['bs'] == 'LIVE' and \
         game['ts'].split(" ")[1] == 'OT':
 
-        print(away_team_name)
-        # tweet_game(api, away_team_name, away_team_score,
-        #             home_team_name, home_team_score)
+        tweet_game(api, away_team_name, away_team_score,
+                    home_team_name, home_team_score)
 
-def tweet_game(api, away_team_name, away_team_score,
-                home_team_name, home_team_score):
 
-    if home_team_score and away_team_score:
-        score = home_team_score.text.lstrip().strip(" ")
-    # if overtime:
-    status = 'Tied at ' + score + away_team_name + ' @ ' + \
-                home_team_name + ' in #3on3OT'
-    print(status)
-    # api.update_status(status=status)
+def tweet_game(api, away_team_ht, away_team_score,
+                home_team_ht, home_team_score):
+
+    if home_team_score == away_team_score:
+        score = home_team_score
+        status = away_team_ht + ' @ ' + home_team_ht + \
+                    ' tied at ' + score + ' in #3on3OT'
+        print(status)
+        api.update_status(status=status)
+
+    else:
+        pass
 
 for game in games:
-    parse_game(game)
+    parse_game(game, hashtags)
